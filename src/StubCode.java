3903644b3f8d8f9637e49ba5d8b300692eacc455
Sav-bit <56634996+Sav-bit@users.noreply.github.com>
2019-11-14 17:48:56 +0100

    added the bin and the src dir for the project + the build file

---
+++ src/StubCode.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author sav
+ *
+ */
+public class StubCode {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
