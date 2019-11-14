7eab6cd440222a8815932c6bfe37811e1ce2b437
Sav-bit <56634996+Sav-bit@users.noreply.github.com>
2019-11-14 18:40:16 +0100

    added the stub code in src

--- src/StubCode.java
+++ src/StubCode.java
@@ -13,7 +13,7 @@ public class StubCode {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("howdy partner, i'm a smally code! :)");
 	}
 
 }
