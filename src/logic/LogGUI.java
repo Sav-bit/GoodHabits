3be6c91edbc1415b3cc999ce552c8e107cd91661
Sav <56634996+Sav-bit@users.noreply.github.com>
2020-02-18 13:03:25 +0100

    added the right title and the css

--- src/logic/LogGUI.java
+++ src/logic/LogGUI.java
@@ -16,8 +16,9 @@ public class LogGUI extends Application {
 	public void start(Stage primaryStage) throws Exception {
 		try	{
 			Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
-			primaryStage.setTitle("AppBar");
+			primaryStage.setTitle("GoodHabits - login");
 		    Scene scene = new Scene(root);
+		    scene.getStylesheets().add(getClass().getResource("goodHabits.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		}catch(Exception e)	{
